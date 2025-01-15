4e0bc489f25f121994c5f879179a741a005df3bb
mentesnot <mentesnotassefa0gmail.com>
2025-01-15 20:43:10 +0000

    final edit

--- quiz.js
+++ quiz.js
@@ -1,1 +1,28 @@
 
+
+
+
+function checkAnswer() {
+
+    const correctAnswer = "4";
+
+    const selectedRadioButton = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = null;
+ 
+     if(selectedRadioButton){
+         userAnswer = selectedRadioButton.value;
+     }
+     
+     const feedbackElement = document.getElementById("feedback");
+
+    if (userAnswer === correctAnswer) {
+        feedbackElement.textContent = "Correct! Well done.";
+    } else {
+        feedbackElement.textContent = "That's incorrect. Try again!";
+    }
+
+};
+const SubmitAnswer = document.getElementById("submit-answer");
+SubmitAnswer.addEventListener('click', checkAnswer);
+
+
